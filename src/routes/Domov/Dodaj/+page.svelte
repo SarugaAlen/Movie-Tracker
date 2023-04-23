3abd334b0b04338626038c4da160d8ad35819931
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2023-04-23 19:08:44 +0200

    fixed add endpoint

--- src/routes/Domov/Dodaj/+page.svelte
+++ src/routes/Domov/Dodaj/+page.svelte
@@ -1,18 +1,24 @@
 <script>
     let naslov = ""
+	let reziser = ""
+	let leto = ""
+	let ocena = 0
     let trajanje = 0
     let ogledan = true
     let result = null
 
     async function ShraniVBazo() {
-        const res = await fetch("http://127.0.0.1:8000/Filmi/Dodaj", {
+        const res = await fetch("http://127.0.0.1:8000/Film", {
 			method: 'POST',
 			headers: {
     		"Content-type": "application/json; charset=UTF-8"
  			 },
 			body: JSON.stringify({
 				Naslov : naslov,
+				Leto : leto,				
 				Trajanje : trajanje,
+				Reziser : reziser,
+				Ocena : ocena,
 				Ogledan : ogledan
 			}),
 		})
@@ -41,10 +47,23 @@
 
 <label for="Naslov">Vnesi naslov filma:</label>
 <input name="Naslov" type="text" bind:value={naslov} />
-<label for="Trajanje">Vnesi naslov filma:</label>
-<input name="Trajanje" type="number" bind:value={trajanje} />
+
+<label for="Leto">Vnesi leto izida filma:</label>
+<input name="Leto" type="number" bind:value={leto} />
+
+<label for="Trajanje">Vnesi trajanje filma:</label>
+<input name="Trajanje" type="string" bind:value={trajanje} />
+
+<label for="Reziser">Vnesi reziserja filma:</label>
+<input name="Reziser" type="text" bind:value={reziser} />
+
+<label for="Ocena">Vnesi oceno filma:</label>
+<input name="Ocena" type="number" bind:value={ocena} />
+
 <label for="Ogledan">Ogledan?</label>
 <input name="Ogledan" type="checkbox" bind:checked={ogledan} />
+
+
 
 
 <button on:click|preventDefault={ShraniVBazo}>Shrani v bazo!</button>
