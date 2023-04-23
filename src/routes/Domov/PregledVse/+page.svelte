ee928a6d574937a90c53f20b32008e2f85b13a5b
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2023-04-23 15:14:20 +0200

    Dodan Create And Delete, Get ne dela še

--- src/routes/Domov/PregledVse/+page.svelte
+++ src/routes/Domov/PregledVse/+page.svelte
@@ -1,42 +1,42 @@
+
 <script>
     let naslov = "Zdravo"
     let ID = 0
     let ogledan = "true"
     let result = null
+	let json
 
-    let povezavaDodaj = "http://127.0.0.1:8000/Filmi/Dodaj"
-    	
-    //Poglej kako se delajo api klici s tem fetchom 
-   //async function ShraniVBazo() {
-   //    const res = await fetch('https://httpbin.org/post', {
-	//		method: 'POST',
-	//		body: JSON.stringify({
-	//			naslov,
-	//			trajanje,
-   //            ogledan
-	//		})
-	//	})
 
-	//	const json = await res.json()
-	//	result = JSON.stringify(json)
-	//}
-
-    async function Izbrisi() {
-        const res = await fetch(povezavaDodaj, {
-			method: 'DELETE',
-			body: JSON.stringify({
-				naslov
-			})
-		})
-
+	async function ShraniVBazo() {
+        const res = await fetch("http://127.0.0.1:8000/Filmi")
 		const json = await res.json()
 		result = JSON.stringify(json)
 	}
 
-
+	//export let data;
+	
 </script>
 
 
 <h1>
     Pregled vseh datotek
 </h1>
+
+<!--
+<div>
+	{#if result}
+	  {#each result as item}
+		<p>{item.naslov}</p>
+	  {/each}
+	{:else}
+	  <p>Loading...</p>
+	{/if}
+</div>
+-->
+
+
+<button on:click={ShraniVBazo}>
+    Pregled vseh datotek
+</button>
+
+
